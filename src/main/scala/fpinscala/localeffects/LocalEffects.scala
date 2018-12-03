6a758998b4750ee4d91a9c26783af4ce12df888c
Antonio Silva <pg11024@gmail.com>
2018-12-03 23:49:18 +0000

    local effects

---
+++ src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -0,0 +1,133 @@
+package fpinscala.localeffects
+
+object Mutable {
+  def quicksort(xs: List[Int]): List[Int] = if (xs.isEmpty) xs else {
+    val arr = xs.toArray
+    def swap(x: Int, y: Int): Unit = {
+      val tmp = arr(x)
+      arr(x) = arr(y)
+      arr(y) = tmp
+    }
+
+    def partition(l: Int, r: Int, pivot: Int): Int = {
+      val pivotVal = arr(pivot)
+      swap(pivot, r)
+      var j = 1
+      for (i <- l until r) if (arr(i) < pivotVal){
+        swap(i, j)
+        j += 1
+      }
+      swap(j, r)
+      j
+    }
+
+    def qs(l: Int, r: Int): Unit = if (l < r) {
+      val pi = partition(l, r, l + (r - l) / 2)
+      qs(l, pi - 1)
+      qs(pi + 1, r)
+    }
+    qs(0, arr.length - 1)
+    arr.toList
+  }
+}
+
+sealed trait ST[S, A] {self =>
+  protected def run(s: S): (A, S)
+
+  def map[B](f: A => B): ST[S, B] = new ST[S, B] {
+    def run(s: S): (B, S) = {
+      val (a, s1) = self.run(s)
+      (f(a), s1)
+    }
+  }
+
+  def flatMap[B](f: A => ST[S, B]): ST[S, B] = new ST[S, B] {
+    def run(s: S): (B, S) = {
+      val (a, s1) = self.run(s)
+      f(a).run(s1)
+    }
+  }
+}
+
+object ST {
+  def apply[S, A](a: => A): ST[S, A] = {
+    lazy val memo = a
+    new ST[S, A] {
+      def run(s: S): (A, S) = (memo, s)
+    }
+  }
+
+  def runST[A](st: RunnableST[A]): A = st[Null].run(null)._1
+}
+
+sealed trait STRef[S,A] {
+  protected var cell: A
+  def read: ST[S,A] = ST(cell)
+  def write(a: => A): ST[S,Unit] = new ST[S,Unit] {
+    override def run(s: S): (Unit, S) = {
+      cell = a
+      ((), s)
+    }
+  }
+}
+
+object STRef {
+  def apply[S, A](a: A): ST[S, STRef[S, A]] = ST(new STRef[S, A] {
+    override var cell: A = a
+  })
+}
+
+trait RunnableST[A] {
+  def apply[S]: ST[S, A]
+}
+
+sealed abstract class STArray[S, A](implicit manifest: Manifest[A]) {
+  protected def value: Array[A]
+  def size: ST[S, Int] = ST(value.length)
+
+  def read(i: Int): ST[S, A] = ST(value(i))
+
+  def write(i: Int, a: A): ST[S, Unit] = new ST[S, Unit] {
+    def run(s: S): (Unit, S) = {
+      value(i) = a
+      ((), s)
+    }
+  }
+
+  def freeze: ST[S, List[A]] = ST(value.toList)
+
+  def fill(xs: Map[Int, A]): ST[S, Unit] = ???
+
+  def swap(i: Int, j: Int): ST[S, Unit] = for {
+    x <- read(i)
+    y <- read(j)
+    _ <- write(i, y)
+    _ <- write(j, x)
+  } yield ()
+}
+
+object STArray {
+  def apply[S, A: Manifest](sz: Int, v: A): ST[S, STArray[S, A]] = ST(new STArray[S, A]{
+    lazy val value: Array[A] = Array.fill(sz)(v)})
+
+  def fromList[S, A: Manifest](xs: List[A]): ST[S, STArray[S, A]] = ST(new STArray[S, A]{
+    override lazy val value: Array[A] = xs.toArray
+  })
+}
+
+object Immutable {
+  def noop[S]: ST[S, Unit] = ST[S, Unit](())
+
+  def partition[S](a: STArray[S, Int], l: Int, r: Int, pivot: Int): ST[S, Int] = ???
+
+  def qs[S](a: STArray[S, Int], l: Int, r: Int): ST[S, Unit] = ???
+
+  def quicksort(xs: List[Int]): List[Int] = if (xs.isEmpty) xs else ST.runST(new RunnableST[List[Int]] {
+    override def apply[S]: ST[S, List[Int]] = for {
+      arr    <- STArray.fromList(xs)
+      size   <- arr.size
+      _      <- qs(arr, 0, size - 1)
+      sorted <- arr.freeze
+    } yield sorted
+  })
+}
